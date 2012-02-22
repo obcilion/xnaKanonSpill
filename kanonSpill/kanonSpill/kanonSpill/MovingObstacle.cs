31a1f2b079cf3d2323903cea0bf0efb71dc0818a
bogjru10 <bogjru10@nith.no>
2012-02-22 13:38:10 +0100

    Solid ferdig? Moving in progress.....

--- kanonSpill/kanonSpill/kanonSpill/MovingObstacle.cs
+++ kanonSpill/kanonSpill/kanonSpill/MovingObstacle.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace kanonSpill
 {
-    class MovingObstacle
+
+    class MovingObstacle : GameObject
     {
+        float height;
+        float width;
+        Vector2 position;
+        float velocity;
+        char axis;
+        float distance;
+        float distanceMoved;
+        
+
+        public MovingObstacle(Texture2D texture, Vector2 position, float height, float width, float velocity, char axis)
+            :base(texture, position)
+        {
+            this.width = width;
+
+
+        }
+
+        public void update()
+        {
+
+
+        }
     }
 }
--- kanonSpill/kanonSpill/kanonSpill/SolidObstacle.cs
+++ kanonSpill/kanonSpill/kanonSpill/SolidObstacle.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace kanonSpill
 {
-    class SolidObstacle
+
+    class SolidObstacle : GameObject
     {
+        float height;
+        float width;
+        Vector2 position;
+
+
+        public SolidObstacle(Texture2D texture, position, float height, float width)
+            :base(texture, position)
+        {
+            
+        }
+
     }
 }

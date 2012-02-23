5f816c3585ec7840bf1645b22929dc01d0230bbb
Jan Arild Brobak <brojan10@nith.no>
2012-02-23 16:05:31 +0100

    H

---
+++ kanonSpill/kanonSpill/kanonSpill/Target.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace CannonGame
+{
+
+    class Target : GameObject
+    {
+
+        public bool ballIsInside = false;
+        
+        public Target(Texture2D texture)
+            : base(texture, Vector2.Zero)
+        {
+
+        }
+
+        
+
+    }
+}
